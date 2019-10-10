c046087dbb5674689ae803cce7727d9cde71fe2f
sonalsannigrahi <sannigrahisonal@gmail.com>
2019-10-10 10:26:09 +0200

    Add first complex number class

---
+++ td4/complex.cpp
@@ -0,0 +1,14 @@
+
+
+class Complex {
+private:
+  double real;        // the real part
+  double imaginary;   // the imaginary part
+
+  public:
+    // construct the complex number zero
+    Complex();
+
+    // construct a complex number with given real and imaginary parts
+    Complex(double real, double imaginary);
+};
