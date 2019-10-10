b62f8f8cdee847f2cadf82ba1243d1631d0aa837
sonalsannigrahi <sannigrahisonal@gmail.com>
2019-10-10 11:00:36 +0200

    Completed Ex1

--- td4/complex.cpp
+++ td4/complex.cpp
@@ -1,14 +1,34 @@
-
+#include <iostream>
 
 class Complex {
-private:
-  double real;        // the real part
-  double imaginary;   // the imaginary part
+  private:
+    double real;        // the real part
+    double imaginary;   // the imaginary part
 
   public:
     // construct the complex number zero
-    Complex();
-
+    Complex(){
+	      real = 0;
+	      imaginary = 0;
+    }
+	
     // construct a complex number with given real and imaginary parts
-    Complex(double real, double imaginary);
+    Complex(double real, double imaginary){
+    	this -> real = real;
+	this -> imaginary = imaginary;
+    }
+    ~Complex() {
+	std::cout << "Destroying number " << real << "+" << imaginary<< "*i" << std::endl;
+    }
+    
+    	
+};
+
+
+int main() {
+	Complex a(0,0);
+	Complex b(3,0);
+	Complex c(1,2);
+	return 0;
+
 };
